194e9bad78793a5363ee9709b673ac2aa9eb0bd8
ZeroMarrow <71882927+ZeroMarrow@users.noreply.github.com>
2021-02-13 21:41:28 +0200

    Update Program.cs

--- Kedr/Program.cs
+++ Kedr/Program.cs
@@ -10,7 +10,7 @@ namespace Kedr
             p.Display();
             DateTime d = new DateTime(2021, 1, 31, 14, 33, 29);
             
-            Trains t = new Trains(14, 11, 22, "324556", "Северное",new DateTime(2021, 1, 31, 14, 33, 29),1);
+            Trains t = new Trains(14, 11, 22, "324556", "Светлогорск",new DateTime(2021, 1, 31, 14, 33, 29),1);
             t.Display();
         }
     }
